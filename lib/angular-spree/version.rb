410348f34f52121ea2b0eb1db488e7e726378be9
Ben A Morgan <ben@benmorgan.io>
2014-10-01 18:11:22 -0700

    expand path to current file directory

--- lib/angular-spree/version.rb
+++ lib/angular-spree/version.rb
@@ -1,5 +1,5 @@
 require 'json'
 
 module AngularSpree
-  VERSION = ::JSON.parse(File.read './../../bower.json')['version']
+  VERSION = ::JSON.parse(File.read "#{File.expand_path File.dirname(__FILE__)}/../../bower.json")['version']
 end
